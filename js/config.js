0981d5547f41e9cc63004a75e5ce3e94b5ad3697
Shahzad A. Chaudhry <shahzad_chaudhry@hotmail.com>
2019-05-15 15:58:45 +0100

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_NB1XTiB4F', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5mupj7kval9i6ugqjdiea2f21i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
